f2934251018f5e6762842f809ec8d986b7cff595
Yigit Muzaffer Yildiz <myildiz1999@gmail.com>
2025-03-17 22:52:20 +0300

    Update Assignment 1 Study.R

--- Assignment 1 Study.R
+++ Assignment 1 Study.R
@@ -1,21 +1,38 @@
 data(mtcars)
 mtcars
 
-x <- c(1, 3, 7, 5, 2)
-x
-compute_stats <- function(x){
-  mean_of_vector <- mean(x)
-  median_of_vector <- median(x)
-  variance_of_vecor <- sum(x[1:length(x)] - mean(x))/(length(x)-1)
-  IQR_of_vector <- IQR(x)
-  minimum_of_vector <- min(x)
-  maximum_of_vector <- max(x)
-  print(mean_of_vector)
-  print(median_of_vector)
-  print(variance_of_vecor)
-  print(IQR_of_vector)
-  print(minimum_of_vector)
-  print(maximum_of_vector)
-}
-compute_stats(x)
+# The function returning a named list containing the mean, median, variance, IQR, minimum, and maximum of the input
 
+compute_stats <- function(x){
+  if (!is.numeric(x)){
+    stop("Input is not numeric vector.") #We make sure that the function takes numeric vector as input.
+  }
+  
+  statistics_of_data <- list(
+  mean =  mean(x),
+  median =  median(x),
+  variance = var(x),
+  IQR = IQR(x),
+  min = min(x),
+  max =  max(x)
+  )
+  
+  statistics_of_data
+}
+
+for (column_name in names(mtcars)){
+  if(is.numeric(mtcars[[column_name]])){
+    computed_statistics <- compute_stats(mtcars[[column_name]])
+    cat("\nStatistics for Column:", column_name, "\n")
+    print(computed_statistics)
+    cat("\n---------------------\n")
+  }
+}
+
+
+numeric_stats <- sapply(mtcars[sapply(mtcars, is.numeric)], compute_stats)
+numeric_stats
+
+matrix_of_mtcars <- as.matrix(mtcars[sapply(mtcars, is.numeric)])
+matrix_of_statistics <- apply(matrix_of_mtcars, MARGIN = 2, compute_stats)
+matrix_of_statistics
