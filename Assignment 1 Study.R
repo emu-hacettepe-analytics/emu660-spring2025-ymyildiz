f2a91de84d370a0d2c6a410ff2f212705ab38bad
Yigit Muzaffer Yildiz <myildiz1999@gmail.com>
2025-03-19 00:29:03 +0300

    Update Assignment 1 Study.R

--- Assignment 1 Study.R
+++ Assignment 1 Study.R
@@ -1,3 +1,6 @@
+
+# (b) section of Assignment 1
+
 data(mtcars)
 mtcars
 
@@ -36,3 +39,41 @@ numeric_stats
 matrix_of_mtcars <- as.matrix(mtcars[sapply(mtcars, is.numeric)])
 matrix_of_statistics <- apply(matrix_of_mtcars, MARGIN = 2, compute_stats)
 matrix_of_statistics
+
+# (c) section of Assignment 1
+
+library(dslabs)
+data(na_example)
+na_example
+
+sum(is.na(na_example)) # Count of NA values found within the dataset
+which(is.na(na_example)) # Index position of NA values in the dataset
+
+mean_of_naexample <- mean(na_example, na.rm = TRUE) # Mean of the dataset
+std_dev_of_naexample <- sqrt(var(na_example, na.rm = TRUE)) # Standard deviation of the dataset
+
+mean_of_naexample
+std_dev_of_naexample
+
+
+nonmissing_naexample <- na_example[!is.na(na_example)] # Let us remove the NA's to find the median of the non-missing values.
+nonmissing_naexample
+median_of_nonmissing <- median(nonmissing_naexample)
+
+# Version 1 where all NA values are replaced with the median of the non-missing values
+na_example_Version1 <- ifelse(is.na(na_example), median_of_nonmissing, na_example)
+na_example_Version1
+
+# Version 2 where all NA values are replaced with the a randomly selected non-missing value
+set.seed(123)  # Set seed for reproducibility
+na_example_Version2 <- na_example
+na_example_Version2[is.na(na_example_Version2)] <- sample(nonmissing_naexample, sum(is.na(na_example_Version2)), replace = TRUE)
+
+na_example_Version2
+
+
+head(na_example_filled)
+
+
+
+
